6cb02f943315f4f9980812de2d1fb8767f3017e8
Nils <nbrast@students.uni-mainz.de>
2019-07-08 01:38:56 +0200

    Und der dummy player

---
+++ 5GewinntDummyPlayer.py
@@ -0,0 +1,18 @@
+import random
+import sys
+import time
+
+if (len(sys.argv)<3):
+    print "5GewinntDummyPlayer.py STATEFILE MOVEFILE"
+    sys.exit()
+
+move = str(random.randint(1,12))
+if (move=='12'):
+	move='flip'
+	move='4'
+
+f = open(sys.argv[2], "w")
+f.write(move)
+f.close()
+
+time.sleep(20)
